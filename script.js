b10800b0877c4a61dd85b0937e58f4f20ea08981
LucasTascaXD <thegamesdead@gmail.com>
2023-02-02 10:54:49 -0300

    attt

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ let icons = document.querySelectorAll('.menu-icon');
 icons.forEach((icon, index) => {
   icon.addEventListener('click', function(){
     let targetMenu = menus[index];
-    if(targetMenu.style.display === "none"){
+    if(targetMenu.style.display === ""){
       targetMenu.style.display = "block";
       icon.innerHTML = "⇩";
     } else {
-      targetMenu.style.display = "none";
+      
+      targetMenu.style.display = "" ;
       icon.innerHTML = "⇨";
     }
   });
